810dc2a7dfb4330bbe33362cd3ed00f867333f62
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-09-17 15:10:57 +0300

    Update modul2_dop.py

--- modul2_dop.py
+++ modul2_dop.py
@@ -1,19 +1,1 @@
-# Bogushev V.V.
 
-while True:
-    key_number = int(input("Введите число от 3 до 20: "))
-    if (key_number > 2) and (key_number < 21):
-        break
-    else:
-         print("Неверно введено число! Повторите попытку!")
-
-str_resault = ""
-first = 1
-while first < key_number:
-    second = first + 1
-    while second < key_number:
-         if key_number % (first + second) == 0:
-              str_resault += str(first) + str(second)
-         second += 1
-    first += 1
-print(f'{key_number} - {str_resault}')
