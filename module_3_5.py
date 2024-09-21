a980369bbbe8a03aa4f03353cc4336681770f9ed
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-09-21 10:49:45 +0300

    рекурсивная функция
    Самостоятельная работа по уроку "Рекурсия"

---
+++ module_3_5.py
@@ -0,0 +1,11 @@
+# Bogushev V.V.
+def get_multiplied_digits(number: int = 0) -> int:
+    str_number = str(number)
+    if len(str_number) < 2:
+        return int(str_number[0])
+    else:
+        first = int(str_number[0])
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+
+print(get_multiplied_digits(44))
