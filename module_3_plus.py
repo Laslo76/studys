b580171ef5c6fcc1a928c89f627d7434f82c4a41
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-09-26 08:52:00 +0300

    add file
    Дополнительное практическое задание по модулю 3

---
+++ module_3_plus.py
@@ -0,0 +1,29 @@
+# Bogushev V.V.
+def calculate_structure_sum(data_structure) -> int:
+    res = 0
+    if type(data_structure) is int:
+        return data_structure
+    elif type(data_structure) is str:
+        return len(data_structure)
+    elif type(data_structure) in (tuple, list, set):
+        for i in data_structure:
+            res += calculate_structure_sum(i)
+        return res
+    elif type(data_structure) is dict:
+        for i in data_structure.keys():
+            res += calculate_structure_sum(i)
+        for i in data_structure.values():
+            res += calculate_structure_sum(i)
+        return res
+    else:
+        raise Exception('Ошибка Типов!')
+
+
+data_structure = [
+                    [1, 2, 3],
+                    {'a': 4, 'b': 5},
+                    (6, {'cube': 7, 'drum': 8}),
+                    "Hello",
+                    ((), [{(2, 'Urban', ('Urban2', 35))}])]
+result = calculate_structure_sum(data_structure)
+print(result)
