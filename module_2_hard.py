318ac01625fd444965129c8640b93d52f02a05be
Laslo76 <bogushev.v@yandex.ru>
2024-10-04 13:48:05 +0300

    module 2_hard добавлен вариант решения

--- module_2_hard.py
+++ module_2_hard.py
@@ -5,15 +5,23 @@ while True:
     if (key_number > 2) and (key_number < 21):
         break
     else:
-         print("Неверно введено число! Повторите попытку!")
+        print("Неверно введено число! Повторите попытку!")
 
-str_resault = ""
+str_result = ""
 first = 1
 while first < key_number:
     second = first + 1
     while second < key_number:
-         if key_number % (first + second) == 0:
-              str_resault += str(first) + str(second)
-         second += 1
+        if key_number % (first + second) == 0:
+            str_result += str(first) + str(second)
+        second += 1
     first += 1
-print(f'{key_number} - {str_resault}')
+print(f'{key_number} - {str_result}')
+
+key_number = int(input('Введите номер от 3 до 20: '))
+result = ''
+for first in range(1, key_number):
+    for second in range(first + 1, key_number):
+        if key_number % (first + second) == 0:
+            result += f'{first}{second}'
+print(f'{key_number} - {result}')
