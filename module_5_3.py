9626e263861da5b23fb4f510a88dd048d0f5dfff
Laslo76 <bogushev.v@yandex.ru>
2024-10-04 12:46:58 +0300

    module 5.3

---
+++ module_5_3.py
@@ -0,0 +1,96 @@
+class House:
+    def __init__(self, name, max_floors):
+        self.name = name
+        self.number_of_floors = max_floors
+
+    def go_to(self, new_floor):
+        if not (new_floor in range(1, self.number_of_floors + 1)):
+            print("Такого этажа не существует!")
+            return
+        for floor in range(1, new_floor + 1):
+            print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if not isinstance(other, House):
+            raise Exception(f'Нельзя сравнивать объекты различных типов!')
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if not isinstance(value, int):
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        if value < 0:
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        if not isinstance(value, int):
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        if value < 0:
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        if not isinstance(value, int):
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        if value < 0:
+            raise Exception(f'Количество этажей должно быть целым, положительным числом!')
+        self.number_of_floors += value
+        return self
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
