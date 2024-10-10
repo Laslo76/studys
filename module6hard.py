b8b8d480cf9e732dd3af128cbcd5b923edea94c9
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2024-10-10 20:52:18 +0300

    final hard modul6

--- module6hard.py
+++ module6hard.py
@@ -4,25 +4,29 @@ from math import pi
 class Figure:
     side_count = 0
     __color = (0, 0, 0)
-    __sides = [0]
+    __sides = []
 
     def __init__(self, *args, **kwargs):
-        self.set_color(*args[0])
-        self.set_sides(list(args[1:]))
+        if isinstance(args[0], tuple) and len(args[0]) == 3:
+            self.set_color(*args[0])
+        else:
+            raise ValueError("No color given")
+        sides = args[1:]
+        if len(sides) == 0:
+            raise ValueError("No sides given")
+        else:
+            self.set_sides(*sides)
 
         if "field" in kwargs.keys():
             self.field = kwargs["field"]
         else:
             self.field = False
 
-    def get_color(self):
-        return self._Figure__color
-
     def __is_valid_color(self, r, g, b):
         result = True
-        result = result and isinstance(r, int) and r > 0
-        result = result and isinstance(g, int) and r > 0
-        result = result and isinstance(b, int) and r > 0
+        result = result and isinstance(r, int) and (r >= 0) and (r < 256)
+        result = result and isinstance(g, int) and (g >= 0) and (g < 256)
+        result = result and isinstance(b, int) and (b >= 0) and (b < 256)
         return result
 
     def set_color(self, r, g, b):
@@ -31,15 +35,25 @@ class Figure:
         else:
             Exception(f'Не задан цвет фигуры!')
 
+    def get_color(self):
+        return self._Figure__color
+
     def __is_valid_side(self, sides: list):
         if len(sides) != self.side_count:
             return False
-
         for side in sides:
             if isinstance(side, int) and (side <= 0):
                 return False
-
         return True
+
+    def set_sides(self, *args):
+        new_sides = args
+        if self.side_count == 0:
+            self.side_count = len(new_sides)
+        if self.side_count != len(new_sides):
+            new_sides = [1 for i in range(self.side_count)]
+        if self.__is_valid_side(new_sides):
+            self.__sides = [x for x in new_sides]
 
     def get_sides(self):
         return self._Figure__sides
@@ -47,23 +61,16 @@ class Figure:
     def __len__(self):
         return sum(self.get_sides())
 
-    def set_sides(self, new_sides):
-        if len(new_sides) != self.side_count:
-            return
-
-        if self.__is_valid_side(new_sides):
-            self.__sides = [x for x in new_sides]
-        else:
-            self.__sides = [1 for i in range(self.side_count)]
-
 
 class Circle(Figure):
     side_count = 1
     __radius = 0
+    __sides = [0]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__radius = self.get_sides()[0] / pi / 2
+        sides = self.get_sides()
+        self.__radius = sides[0] / pi / 2
 
     def get_square(self):
         return pi * self._Circle__radius ** 2
@@ -71,7 +78,7 @@ class Circle(Figure):
 
 class Triangle(Figure):
     side_count = 3
-
+    __sides = [0, 0, 0]
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -86,15 +93,16 @@ class Triangle(Figure):
 
 class Cube(Figure):
     side_count = 12
+    __sides = [0 for i in range(12)]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def set_sides(self, new_sides):
-
-        valid = self._Figure__is_valid_side(new_sides)
-        self.__sides = [new_sides[0] for i in range(self.side_count)]
-
+    def set_sides(self, *args):
+        if len(args) == 1:
+            self._Figure__sides = [args[0] for x in range(self.side_count)]
+        elif len(args) == 12:
+            self._Figure__sides = [x for x in range(self.side_count)]
 
     def get_volume(self):
         return self.get_sides()[0] ** 3
@@ -124,5 +132,5 @@ if __name__ == "__main__":
     # Проверка объёма (куба):
     print(cube1.get_volume())
 
-
-
+    triangle1 = Triangle((0, 0, 0),3, 4, 5)
+    print(triangle1.get_square())
