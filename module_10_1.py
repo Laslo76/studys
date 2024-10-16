0a42360e8af93d0d603708221b31f0231061c6a1
Laslo76 <bogushev.v@yandex.ru>
2024-10-16 20:49:54 +0300

    edit module_10_1

--- module_10_1.py
+++ module_10_1.py
@@ -1,8 +1,8 @@
 # Bogushev V.V.
-import datetime
 from time import sleep
 from datetime import datetime
 from threading import Thread
+
 
 def write_words(word_count, file_name):
     if file_name == "":
@@ -14,28 +14,29 @@ def write_words(word_count, file_name):
     print(f"Завершилась запись в файл {file_name}")
 
 
-start_time = datetime.now()
-write_words(10, "example1.txt")
-write_words(30, "example2.txt")
-write_words(200, "example3.txt")
-write_words(100, "example4.txt")
-long_time = datetime.now() - start_time
-print(f"Работа потоков {long_time}")
+if __name__ == '__main__':
+    start_time = datetime.now()
+    write_words(10, "example1.txt")
+    write_words(30, "example2.txt")
+    write_words(200, "example3.txt")
+    write_words(100, "example4.txt")
+    long_time = datetime.now() - start_time
+    print(f"Работа потоков {long_time}")
 
-start_time = datetime.now()
-first_threading = Thread(target=write_words, args=(10, "example5.txt"))
-second_threading = Thread(target=write_words, args=(30, "example6.txt"))
-third_threading = Thread(target=write_words, args=(200, "example7.txt"))
-fourth_threading = Thread(target=write_words, args=(100, "example8.txt"))
+    start_time = datetime.now()
+    first_threading = Thread(target=write_words, args=(10, "example5.txt"))
+    second_threading = Thread(target=write_words, args=(30, "example6.txt"))
+    third_threading = Thread(target=write_words, args=(200, "example7.txt"))
+    fourth_threading = Thread(target=write_words, args=(100, "example8.txt"))
 
-first_threading.start()
-second_threading.start()
-third_threading.start()
-fourth_threading.start()
+    first_threading.start()
+    second_threading.start()
+    third_threading.start()
+    fourth_threading.start()
 
-first_threading.join()
-second_threading.join()
-third_threading.join()
-fourth_threading.join()
-long_time = datetime.now() - start_time
-print(f"Работа потоков {long_time}")
+    first_threading.join()
+    second_threading.join()
+    third_threading.join()
+    fourth_threading.join()
+    long_time = datetime.now() - start_time
+    print(f"Работа потоков {long_time}")
