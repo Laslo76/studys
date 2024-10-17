e6ebb981bbe58c496835c5d4a29df461a38958f5
Laslo76 <bogushev.v@yandex.ru>
2024-10-17 21:05:52 +0300

    add module_10_6

---
+++ module_10_6.py
@@ -0,0 +1,29 @@
+import multiprocessing
+import datetime
+
+def worker(data):
+    result = data ** 2
+    return result
+
+
+if __name__ == '__main__':
+
+    data = [x for x in range(1, 1_500_001)]
+    start_ = datetime.datetime.now()
+    result = list(map(worker, data))
+    stop_ = datetime.datetime.now()
+    print(f'время обычное - {stop_ - start_}')
+    #print(result)
+
+    data = [x for x in range(1, 1_500_001)]
+    start_ = datetime.datetime.now()
+
+    pool = multiprocessing.Pool(processes=2)
+    #result = pool.map(worker, data)
+
+    pool.close()
+    pool.join()
+
+    stop_ = datetime.datetime.now()
+    print(f'время мультипроцессорное - {stop_ - start_}')
+    #print(result)
